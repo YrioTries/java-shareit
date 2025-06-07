d0b9960d18264e57d336b6940a3e0fddd9c0ee19
Balagur Dmitry <farnitin@yandex.ru>
2025-06-07 15:37:33 +0400

    edit item and User

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,5 +1,6 @@
 package ru.practicum.shareit.item.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import ru.practicum.shareit.request.ItemRequest;
 import ru.practicum.shareit.user.model.User;
@@ -8,6 +9,7 @@ import ru.practicum.shareit.user.model.User;
  * TODO Sprint add-controllers.
  */
 @Data
+@AllArgsConstructor
 public class Item {
     private long id;
     private String name;
--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -1,13 +1,22 @@
 package ru.practicum.shareit.user.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.lang.Nullable;
 
 /**
  * TODO Sprint add-controllers.
  */
 @Data
+@AllArgsConstructor
 public class User {
     private long id;
+    @NotBlank
+    @NotEmpty
     private String name;
+    @Email
     private String email;
 }
