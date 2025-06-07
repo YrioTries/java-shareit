0f785f5ee9805e139eef84e77b9b3c45090b1b67
Balagur Dmitry <farnitin@yandex.ru>
2025-06-07 13:00:57 +0400

    add Status enum

---
+++ src/main/java/ru/practicum/shareit/booking/enums/Status.java
@@ -0,0 +1,24 @@
+package ru.practicum.shareit.booking.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum Status {
+    WAITING("WAITING"),
+    APPROVED("APPROVED"),
+    REJECTED("REJECTED"),
+    CANCELED("CANCELED");
+
+    private final String status;
+
+    public static boolean isCorrectStatus(String testedValue) {
+        for (Status status : values()) {
+            if (status.status.equals(testedValue)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
