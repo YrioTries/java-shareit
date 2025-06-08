bf7cb54d9c817a8222dcc64edfe286d8deef6924
Balagur Dmitry <farnitin@yandex.ru>
2025-06-08 14:40:20 +0400

    add ErrorHandler

---
+++ src/main/java/ru/practicum/shareit/exception/handling/ErrorHandler.java
@@ -0,0 +1,72 @@
+package ru.practicum.shareit.exception.handling;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import ru.practicum.shareit.exception.ConflictException;
+import ru.practicum.shareit.exception.InternalServerException;
+import ru.practicum.shareit.exception.NotFoundException;
+import ru.practicum.shareit.exception.ValidationException;
+
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler(ValidationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse validationException(final ValidationException e) {
+        return new ErrorResponse("ERROR[400]: Произошла ошибка ValidationException: ", e.getMessage());
+    }
+
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse httpMediaTypeNotSupported(final HttpMediaTypeNotSupportedException e) {
+        return new ErrorResponse("ERROR[400]: Произошла ошибка HttpMediaTypeNotSupportedException: ", e.getMessage());
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse httpMessageNotReadable(final HttpMessageNotReadableException e) {
+        return new ErrorResponse("ERROR[400]: Произошла ошибка HttpMessageNotReadableException: ", e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse argumentTypeMismatch(final MethodArgumentTypeMismatchException e) {
+        return new ErrorResponse("ERROR[400]: Произошла ошибка MethodArgumentTypeMismatchException: ", e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse argumentNotValid(MethodArgumentNotValidException e) {
+        return new ErrorResponse("ERROR[400]: Произошла ошибка MethodArgumentTypeMismatchException: ", e.getMessage());
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse notFoundException(final NotFoundException e) {
+        return new ErrorResponse("ERROR[404]: Произошла ошибка NotFoundException: ", e.getMessage());
+    }
+
+    @ExceptionHandler(ConflictException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErrorResponse conflict(ConflictException e) {
+        return new ErrorResponse("ERROR[409]: Произошла ошибка Conflict: ", e.getMessage());
+    }
+
+    @ExceptionHandler(InternalServerException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse internalServerException(final InternalServerException e) {
+        return new ErrorResponse("ERROR[500]: Произошла ошибка InternalServerException: ", e.getMessage());
+    }
+
+    @ExceptionHandler(Throwable.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse throwableError(final Throwable e) {
+        return new ErrorResponse("ERROR[500]: Произошла ошибка Throwable: ", e.getMessage());
+    }
+}
