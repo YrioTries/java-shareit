9bff2031a056cac2396a5d58d8d4344f9cad0ec8
Balagur Dmitry <farnitin@yandex.ru>
2025-06-08 14:45:46 +0400

    add method get in UserServiceImpl

--- src/main/java/ru/practicum/shareit/user/UserServiceImpl.java
+++ src/main/java/ru/practicum/shareit/user/UserServiceImpl.java
@@ -1,16 +1,25 @@
 package ru.practicum.shareit.user;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import ru.practicum.shareit.user.model.User;
 
+import java.util.ArrayList;
+
+@Qualifier("InMemoryService")
 public class UserServiceImpl implements UserService {
 
     InMemoryUserStorage storage;
 
-    public UserServiceImpl(InMemoryUserStorage storage) {
+    public UserServiceImpl(@Qualifier("InMemoryUserStorage") InMemoryUserStorage storage) {
         this.storage = storage;
     }
 
+    public ArrayList<User> get() {
+        return storage.get();
+    }
+
     public User create(User user) {
+
         return storage.create(user);
     }
 
