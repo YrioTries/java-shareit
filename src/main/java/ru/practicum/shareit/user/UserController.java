ff0fb7eaab16bc970bbcbfacea8ef00a2a7e6b1b
Balagur Dmitry <farnitin@yandex.ru>
2025-06-08 15:21:42 +0400

    add ResponseStatus in UserController

--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -1,6 +1,8 @@
 package ru.practicum.shareit.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.user.model.User;
 
@@ -10,24 +12,29 @@ import ru.practicum.shareit.user.model.User;
 @RestController
 @RequestMapping(path = "/users")
 public class UserController {
-    UserService service;
 
-    public UserController(@Qualifier("InMemoryService") UserService service) {
+    private final UserService service;
+
+    @Autowired
+    public UserController(UserService service) {
         this.service = service;
     }
 
     @PostMapping
-    public User create(User user) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public User create(@RequestBody User user) {
         return service.create(user);
     }
 
     @PatchMapping
-    public User update(User user) {
+    @ResponseStatus(HttpStatus.OK)
+    public User update(@RequestBody User user) {
         return service.update(user);
     }
 
     @DeleteMapping
-    public void delete(long id) {
+    @ResponseStatus(HttpStatus.OK)
+    public void delete(@RequestBody long id) {
         service.delete(id);
     }
 
