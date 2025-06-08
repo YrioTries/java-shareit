72f82488b6893931c89471696e8888b5053231dc
Balagur Dmitry <farnitin@yandex.ru>
2025-06-08 15:22:14 +0400

    add validation to create method

--- src/main/java/ru/practicum/shareit/user/UserServiceImpl.java
+++ src/main/java/ru/practicum/shareit/user/UserServiceImpl.java
@@ -1,15 +1,18 @@
 package ru.practicum.shareit.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import ru.practicum.shareit.exception.ConflictException;
+import ru.practicum.shareit.exception.NotFoundException;
 import ru.practicum.shareit.user.model.User;
 
 import java.util.ArrayList;
 
-@Qualifier("InMemoryService")
 public class UserServiceImpl implements UserService {
 
-    InMemoryUserStorage storage;
+    private final InMemoryUserStorage storage;
 
+    @Autowired
     public UserServiceImpl(@Qualifier("InMemoryUserStorage") InMemoryUserStorage storage) {
         this.storage = storage;
     }
@@ -19,6 +22,17 @@ public class UserServiceImpl implements UserService {
     }
 
     public User create(User user) {
+        if (storage.getUserIds().contains(user.getId()))
+            throw new ConflictException("Такой пользователь уже существует");
+
+        if (storage.get()
+                .stream()
+                .anyMatch(userStream -> userStream.getEmail().equals(user.getEmail()))) {
+            throw new ConflictException("Пользователь с такой почтой уже существует");
+        }
+
+        if (user.getEmail() == null)
+            throw new NotFoundException("Создание пользователя без почты");
 
         return storage.create(user);
     }
