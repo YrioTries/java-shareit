72855ab55e45a604c5f84b2d7db8b0d5a79c231b
Dmitry <farnitin@yandex.ru>
2025-07-16 05:37:50 +0400

    add UserRepository

--- src/main/java/ru/practicum/shareit/user/UserRepository.java
+++ src/main/java/ru/practicum/shareit/user/UserRepository.java
@@ -3,5 +3,14 @@ package ru.practicum.shareit.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.shareit.user.model.User;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    User createUser(User user);
+    User updateUser(User user);
+    User getUserById(Long userId);
+    List<User> getAllUsers();
+    void deleteUser(Long userId);
+    boolean existsByEmail(String email);
+    boolean existsByEmailAndIdNot(String email, Long id);
 }
