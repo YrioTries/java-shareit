6ef1649eccb32b70fff74c54d2f7fa71d453c12f
Dmitry <farnitin@yandex.ru>
2025-07-17 20:49:32 +0400

    add ItemRepository

--- src/main/java/ru/practicum/shareit/item/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/ItemRepository.java
@@ -1,7 +1,24 @@
 package ru.practicum.shareit.item;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.practicum.shareit.item.model.Item;
+import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
+    List<Item> findByOwnerId(Long ownerId);
+    @Query("SELECT i FROM Item i " +
+            "WHERE i.available = true AND " +
+            "(LOWER(i.name) LIKE LOWER(CONCAT('%', :text, '%')) OR " +
+            "LOWER(i.description) LIKE LOWER(CONCAT('%', :text, '%')))")
+    List<Item> searchAvailableItems(String text);
+
+    List<Item> findByRequestId(Long requestId);
+
+    @Query("SELECT i FROM Item i " +
+            "WHERE i.request IS NOT NULL AND " +
+            "i.request.id IN :requestIds")
+    List<Item> findByRequestIds(List<Long> requestIds);
+
+    boolean existsByIdAndOwnerId(Long itemId, Long ownerId);
 }
