4110436b13c9c3828f103273ae3481a9700cf257
Balagur Dmitry <farnitin@yandex.ru>
2025-08-19 05:21:39 +0400

    add ItemRequestMapper

---
+++ shareit-server/src/main/java/ru/practicum/shareit/entity/itemRequest/ItemRequestMapper.java
@@ -0,0 +1,18 @@
+package ru.practicum.shareit.entity.itemRequest;
+
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.Mapper;
+import ru.practicum.shareit.entity.item.dto.ItemResponseDto;
+import java.util.List;
+
+@Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.CONSTRUCTOR)
+public interface ItemRequestMapper {
+
+    ItemRequestDto toItemRequestDto(ItemRequest itemRequest);
+
+    default ItemRequestDto toItemRequestDtoWithItems(ItemRequest itemRequest, List<ItemResponseDto> items) {
+        ItemRequestDto dto = toItemRequestDto(itemRequest);
+        dto.setItems(items);  // Предполагает, что у вас есть сеттер для `items`
+        return dto;
+    }
+}
