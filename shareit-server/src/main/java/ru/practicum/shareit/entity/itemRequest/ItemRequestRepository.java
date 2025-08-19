f976adc8b1d792769636037b2bfe833041a4178a
Balagur Dmitry <farnitin@yandex.ru>
2025-08-19 05:21:19 +0400

    add ItemRequestController

---
+++ shareit-server/src/main/java/ru/practicum/shareit/entity/itemRequest/ItemRequestRepository.java
@@ -0,0 +1,13 @@
+package ru.practicum.shareit.entity.itemRequest;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ItemRequestRepository extends JpaRepository<ItemRequest, Long> {
+
+    List<ItemRequest> findByRequesterIdOrderByCreatedDesc(Long requesterId);
+
+    List<ItemRequest> findByRequesterIdNotOrderByCreatedDesc(Long requesterId, Pageable pageable);
+}
